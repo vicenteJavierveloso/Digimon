0398f4a72733f35d5f9726b1c3b926c307cbfb86
vicenteJavierveloso <vicentejavier.veloso@alumnos.ulagos.cl>
2023-05-02 21:38:28 -0400

    Finalizado programa

--- Main.py
+++ Main.py
@@ -1,108 +1,141 @@
 import sys
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QVBoxLayout, QGridLayout, QWidget, QHBoxLayout, QLineEdit, QSpinBox, QComboBox
+from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication, QSpinBox, QLineEdit, QComboBox, QHBoxLayout, QGridLayout, QWidget, QListWidget, QMessageBox, QPushButton, QListWidgetItem
+
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Digimon")
-        tipos = ["Free", "Vaccine", "Virus", "Data"]
-        stage = []
-        atributo = []
+        self.setFixedSize(QSize(700,300))
 
-        #Elementos
-        #- Labels
-        self.texto_id = QLabel("ID")
+        tipos = ["Free", "Vaccine", "Virus", "Data"]
+        stage = ["Baby", "In-Training", "Rookie", "Champion", "Ultimate", "Mega"]
+        atributo = ["Fire", "Water", "Plant", "Electric", "Wind", "Earth"]
+        
+        #Imagen
+        self.imagen = QLabel()
+        archivo = QPixmap("imagen.png")
+        archivo = archivo.scaled(200,200)
+        self.imagen.setPixmap(archivo)
+
+        #Elementos Texto
+        #Basicos
+        self.texto_ID = QLabel("ID")
         self.texto_nombre = QLabel("Nombre")
         self.texto_stage = QLabel("Stage")
         self.texto_tipo = QLabel("Tipo")
         self.texto_atributo = QLabel("Atributo")
         self.texto_memoria = QLabel("Memoria")
+        #Stats
         self.texto_HP = QLabel("HP")
-        self.texto_SP = QLabel("SP")
         self.texto_Atk = QLabel("Atk")
+        self.texto_Sp = QLabel("Sp")
         self.texto_Def = QLabel("Def")
         self.texto_Int = QLabel("Int")
         self.texto_Spd = QLabel("Spd")
-        #- Entradas
-        #Para usar el contenido de la lista al presionar un boton, usar
-        #el metodo self.entrada_<nombre>.currentText() -> str
-        self.entrada_id = QSpinBox()
+
+        #Entradas
+        #Basicos
+        self.entrada_ID = QSpinBox()
         self.entrada_nombre = QLineEdit()
         self.entrada_stage = QComboBox()
         self.entrada_tipo = QComboBox()
         self.entrada_atributo = QComboBox()
         self.entrada_memoria = QSpinBox()
+        #Stats
         self.entrada_HP = QSpinBox()
-        self.entrada_SP = QSpinBox()
         self.entrada_Atk = QSpinBox()
+        self.entrada_Sp = QSpinBox()
         self.entrada_Def = QSpinBox()
         self.entrada_Int = QSpinBox()
         self.entrada_Spd = QSpinBox()
 
-         # + Inserta la lista de stage al menu
+        #Fijar maximos 
+        self.entrada_HP.setMaximum(9999)
+        self.entrada_Atk.setMaximum(9999)
+        self.entrada_Sp.setMaximum(9999)
+        self.entrada_Def.setMaximum(9999)
+        self.entrada_Int.setMaximum(9999)
+        self.entrada_Spd.setMaximum(9999)
+
+        self.entrada_ID.setMaximum(249)
+
+        #Fijar elementos de menus desplegables
         self.entrada_stage.insertItems(0,stage)
-        # + Inserta la lista de tipos en el menu desplegable
         self.entrada_tipo.insertItems(0,tipos)
-        # + Inserta la lista de atributos al menu desplegable
         self.entrada_atributo.insertItems(0,atributo)
 
-        #Elemento de prueba
-        self.texto_prueba = QLabel("Texto Prueba")
+        #Lista de elementos
+        self.lista = QListWidget()
 
-        #Contenedores
-        #- VerticalGrid
-        basicos = QGridLayout()
-        stats = QGridLayout()
+        #Boton
+        agregar = QPushButton("Añadir")
+        agregar.clicked.connect(lambda: self.agregar(self.entrada_ID.value(), 
+        self.entrada_nombre.text(), self.entrada_stage.currentText(), self.entrada_tipo.currentText(),
+        self.entrada_atributo.currentText(), self.entrada_memoria.value(),
+        self.entrada_HP.value(), self.entrada_Sp.value(), self.entrada_Atk.value(),
+        self.entrada_Def.value(),self.entrada_Int.value(),self.entrada_Spd.value()))
 
-        #- Visualizador
-        visual = QVBoxLayout()
-        visual.addWidget(self.texto_prueba)
+        #Layouts
+        contenedor_basicos = QGridLayout()
+        contenedor_stats = QGridLayout()
+        contenedor_grande = QHBoxLayout()
+        
+        #Añadir elementos
+        #Basicos
+        #Labels
+        contenedor_basicos.addWidget(self.texto_ID,0,0)
+        contenedor_basicos.addWidget(self.texto_nombre,1,0)
+        contenedor_basicos.addWidget(self.texto_stage,2,0)
+        contenedor_basicos.addWidget(self.texto_tipo,3,0)
+        contenedor_basicos.addWidget(self.texto_atributo,4,0)
+        contenedor_basicos.addWidget(self.texto_memoria,5,0)
+        #Entradas
+        contenedor_basicos.addWidget(self.entrada_ID,0,1)
+        contenedor_basicos.addWidget(self.entrada_nombre,1,1)
+        contenedor_basicos.addWidget(self.entrada_stage,2,1)
+        contenedor_basicos.addWidget(self.entrada_tipo,3,1)
+        contenedor_basicos.addWidget(self.entrada_atributo,4,1)
+        contenedor_basicos.addWidget(self.entrada_memoria,5,1)
+        
+        #Stats
+        #Labels
+        contenedor_stats.addWidget(self.texto_HP,0,0)
+        contenedor_stats.addWidget(self.texto_Atk,1,0)
+        contenedor_stats.addWidget(self.texto_Sp,2,0)
+        contenedor_stats.addWidget(self.texto_Def,3,0)
+        contenedor_stats.addWidget(self.texto_Int,4,0)
+        contenedor_stats.addWidget(self.texto_Spd,5,0)
+        #Entradas
+        contenedor_stats.addWidget(self.entrada_HP,0,1)
+        contenedor_stats.addWidget(self.entrada_Atk,1,1)
+        contenedor_stats.addWidget(self.entrada_Sp,2,1)
+        contenedor_stats.addWidget(self.entrada_Def,3,1)
+        contenedor_stats.addWidget(self.entrada_Int,4,1)
+        contenedor_stats.addWidget(self.entrada_Spd,5,1)
+        
+        #Boton
+        contenedor_stats.addWidget(agregar,6,1)
 
-        #- Layout principal
-        layout_principal = QHBoxLayout()
-        layout_principal.addLayout(basicos)
-        layout_principal.addLayout(stats)
-        layout_principal.addLayout(visual)
+        contenedor_grande.addWidget(self.imagen)
 
-        #- Contenedor principal
-        contendor_principal = QWidget()
-        contendor_principal.setLayout(layout_principal)
+        contenedor_grande.addLayout(contenedor_basicos)
+        contenedor_grande.addLayout(contenedor_stats)
+        
+        contenedor_grande.addWidget(self.lista)
+        
+        ventana = QWidget()
+        ventana.setLayout(contenedor_grande)
+        self.setCentralWidget(ventana)
 
-        #Fijar widgets en contenedores (basicos)
-        basicos.addWidget(self.texto_id,0,0)
-        basicos.addWidget(self.texto_nombre,1,0)
-        basicos.addWidget(self.texto_stage,2,0)
-        basicos.addWidget(self.texto_tipo,3,0)
-        basicos.addWidget(self.texto_atributo,4,0)
-        basicos.addWidget(self.texto_memoria,5,0)
-        # + Entradas
-        basicos.addWidget(self.entrada_id,0,1)
-        basicos.addWidget(self.entrada_nombre,1,1)
-        basicos.addWidget(self.entrada_stage,2,1)    
-        basicos.addWidget(self.entrada_tipo,3,1)
-        basicos.addWidget(self.entrada_atributo,4,1)        
-        basicos.addWidget(self.entrada_memoria,5,1)
-        #Fijar widgets en contendores (stats)
-        stats.addWidget(self.texto_HP,0,0)
-        stats.addWidget(self.texto_SP,1,0)
-        stats.addWidget(self.texto_Atk,2,0)
-        stats.addWidget(self.texto_Def,3,0)
-        stats.addWidget(self.texto_Int,4,0)
-        stats.addWidget(self.texto_Spd,5,0)
-        # + Entradas
-        stats.addWidget(self.entrada_HP,0,1)
-        stats.addWidget(self.entrada_SP,1,1)
-        stats.addWidget(self.entrada_Atk,2,1)
-        stats.addWidget(self.entrada_Def,3,1)
-        stats.addWidget(self.entrada_Int,4,1)
-        stats.addWidget(self.entrada_Spd,5,1)
-
-        self.setCentralWidget(contendor_principal)
+    def agregar(self, id, nombre, stage, tipo, atributo, memoria, hp, sp, atk, df, Int, spd):
+        mono = str(str(id) + ' ' + nombre + ' ' + stage + ' ' + tipo + ' ' + atributo + ' ' + str(memoria) + ' ' + str(hp) + ' ' + str(sp) + ' ' + str(atk) + ' ' + str(df) + ' ' + str(Int) + ' ' + str(spd))
+        self.lista.addItem(mono)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana = VentanaPrincipal()
     ventana.show()
-
     app.exec()
